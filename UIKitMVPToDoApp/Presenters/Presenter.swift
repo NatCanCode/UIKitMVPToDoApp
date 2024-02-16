20e228092d695ace38816d53e63e06088b5b2205
NatCanCode <nathalie.piat1@gmail.com>
2024-02-16 10:43:57 +0100

    Add delete button constraints in Storyboard

--- UIKitMVPToDoApp/Presenters/Presenter.swift
+++ UIKitMVPToDoApp/Presenters/Presenter.swift
@@ -33,10 +33,28 @@ class Presenter {
 //        presenterProtocol?.getSentencesProtocol(sentences: sentences)
 //    }
 
+    // GET method
     func getTasks() {
         service.getRecords { tasks, error in
             guard let tasks = tasks else { return }
             self.presenterProtocol?.getAllTasksProtocol(tasks: tasks)
+        }
+    }
+
+    // DELETE method
+    func deleteTask(recordId: String) {
+        service.deleteRecord(recordId: recordId) { error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Error deleting task: \(error)")
+                    // Handle error if needed
+                } else {
+                    print("Task deleted successfully")
+                    // Perform any UI updates or other actions after successful deletion
+                    // For example, you might want to refresh the task list after deletion
+                    self.getTasks()
+                }
+            }
         }
     }
 }
---
+++ UIKitMVPToDoApp/Views/DeleteButton.swift
@@ -0,0 +1,25 @@
+//
+//  DeleteButton.swift
+//  UIKitMVPToDoApp
+//
+//  Created by N N on 16/02/2024.
+//
+
+import UIKit
+
+class DeleteButton: UIButton {
+  
+    @IBAction func deleteButton(_ sender: Any) {
+    }
+    
+//    let service = Service()
+//
+//    // Replace "yourRecordId" with the actual record ID you want to delete
+//    service.deleteRecord(recordId: "yourRecordId") { error in
+//        if let error = error {
+//            print("Error deleting record: \(error)")
+//        } else {
+//            print("Record deleted successfully")
+//        }
+//    }
+}
